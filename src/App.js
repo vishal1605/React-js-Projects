5abe37b1b8ed79036cc76c7ab020a89e2b7d8451
vishal1605 <vishalgt581@gmail.com>
2022-06-03 10:05:35 +0530

    loading cart error fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom';
-import IncreaseCounter from './experimentFile/IncreaseCounter';
+// import IncreaseCounter from './experimentFile/IncreaseCounter';
 import Parent from './cart/Parent'
 
 function App() {
